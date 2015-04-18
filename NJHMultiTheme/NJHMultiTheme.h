2a9009f33149a3e05f1477c46c70316ecde95977
Nathan Hosselton <nathanhosselton@gmail.com>
2015-04-18 14:37:28 -0500

    Initial copy from origin proj

--- NJHMultiTheme/NJHMultiTheme.h
+++ NJHMultiTheme/NJHMultiTheme.h
@@ -1,16 +1,6 @@
-//
-//  NJHMultiTheme.h
-//  NJHMultiTheme
-//
-//  Created by Nathan Hosselton on 4/18/15.
-//  Copyright (c) 2015 Nathan Hosselton. All rights reserved.
-//
-
 #import <AppKit/AppKit.h>
 
 @interface NJHMultiTheme : NSObject
-
-+ (instancetype)sharedPlugin;
-
 @property (nonatomic, strong, readonly) NSBundle* bundle;
++ (instancetype)sharedPlugin;
 @end
--- NJHMultiTheme/NJHMultiTheme.m
+++ NJHMultiTheme/NJHMultiTheme.m
@@ -1,24 +1,29 @@
-//
-//  NJHMultiTheme.m
-//  NJHMultiTheme
-//
-//  Created by Nathan Hosselton on 4/18/15.
-//  Copyright (c) 2015 Nathan Hosselton. All rights reserved.
-//
-
 #import "NJHMultiTheme.h"
 
+
 static NJHMultiTheme *sharedPlugin;
+static NSString *kPluginIdentifier = @"NJHLanguageTheme";
+static NSString *kGeneralUIChange = @"DVTFontAndColorGeneralUISettingsChangedNotification";
+static NSString *kSourceTextChange = @"DVTFontAndColorSourceTextSettingsChangedNotification";
+static NSString *kFileChange = @"transition from one file to another";
+
+typedef NS_ENUM(NSInteger, LTFileType) {
+    LTFileTypeObjC = 1,
+    LTFileTypeSwift
+};
+
 
 @interface NJHMultiTheme()
-
 @property (nonatomic, strong, readwrite) NSBundle *bundle;
 @end
 
-@implementation NJHMultiTheme
+@implementation NJHMultiTheme {
+    LTFileType currentFileType;
+    NSString *objcTheme;
+    NSString *swiftTheme;
+}
 
-+ (void)pluginDidLoad:(NSBundle *)plugin
-{
++ (void)pluginDidLoad:(NSBundle *)plugin {
     static dispatch_once_t onceToken;
     NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
     if ([currentApplicationName isEqual:@"Xcode"]) {
@@ -28,41 +33,75 @@ static NJHMultiTheme *sharedPlugin;
     }
 }
 
-+ (instancetype)sharedPlugin
-{
++ (instancetype)sharedPlugin {
     return sharedPlugin;
 }
 
-- (id)initWithBundle:(NSBundle *)plugin
-{
+- (id)initWithBundle:(NSBundle *)plugin {
     if (self = [super init]) {
         // reference to plugin's bundle, for resource access
         self.bundle = plugin;
-        
-        // Create menu items, initialize UI, etc.
-
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
-        if (menuItem) {
-            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-            [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
-        }
+//        DVTPreferenceSetManager *mgr = [DVTFontAndColorTheme preferenceSetsManager];
+//        mgr.currentPreferenceSet = mgr.userPreferenceSets.firstObject;
+//        NSLog(@"%@", ((DVTPreferenceSetManager *)[DVTFontAndColorTheme preferenceSetsManager]).availablePreferenceSets);
+//        NSLog(@"%@", ((DVTPreferenceSetManager *)[DVTFontAndColorTheme preferenceSetsManager]).userPreferenceSets);
+        // Listener for theme changes
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(themeDidChange:) name:kGeneralUIChange object:nil];
+        // Listener for file changes
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fileDidChange:) name:kFileChange object:nil];
     }
     return self;
 }
 
-// Sample Action, for menu item:
-- (void)doMenuAction
-{
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, World"];
-    [alert runModal];
+- (void)fileDidChange:(NSNotification *)note {
+    if (![note.object count])
+        return;
+
+    id loc = note.object[@"next"];
+    SEL selector = NSSelectorFromString(@"documentURLString");
+    IMP imp = [loc methodForSelector:selector];
+    id (*func)(id, SEL) = (void *)imp;
+    NSString *fileURLString = func(loc, selector);
+    LTFileType nextType;
+
+    if ([[fileURLString substringWithRange:NSMakeRange(fileURLString.length - 2, 2)] isEqualToString:@".h"] ||
+        [[fileURLString substringWithRange:NSMakeRange(fileURLString.length - 2, 2)] isEqualToString:@".m"])
+        nextType = LTFileTypeObjC;
+    else if ([[fileURLString substringWithRange:NSMakeRange(fileURLString.length - 6, 6)] isEqualToString:@".swift"])
+        nextType = LTFileTypeSwift;
+
+    if (!nextType || nextType == currentFileType) //NOTE: Non-native source files will maintain current theme
+        return;
+
+    switch (nextType) {
+        case LTFileTypeObjC:
+            // Set ObjC Theme
+            break;
+        case LTFileTypeSwift:
+            // Set Swift Theme
+            break;
+    }
+
+    currentFileType = nextType;
 }
 
-- (void)dealloc
-{
+-(void)themeDidChange:(NSNotification *)note {
+    NSLog(@"Theme changed");
+}
+
+- (NSMutableDictionary *)defaultz {
+    return [[[NSUserDefaults standardUserDefaults] persistentDomainForName:kPluginIdentifier] mutableCopy];
+}
+
+- (void)loadPreferences {
+    //
+}
+
+- (void)savePreferences {
+    //
+}
+
+- (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
