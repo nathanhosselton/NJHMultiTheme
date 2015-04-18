98b7813f4a2b8c9afa5fe7ee81118f93390cf9f0
Nathan Hosselton <nathanhosselton@gmail.com>
2015-04-18 17:14:06 -0500

    Change themes on source file switch

--- NJHMultiTheme/NJHMultiTheme.m
+++ NJHMultiTheme/NJHMultiTheme.m
@@ -2,9 +2,10 @@
 
 
 static NJHMultiTheme *sharedPlugin;
-static NSString *kPluginIdentifier = @"NJHLanguageTheme";
+static NSString *kPluginIdentifier = @"NJHMultiTheme";
+static NSString *kObjcTheme = @"ObjcThemeName";
+static NSString *kSwiftTheme = @"SwiftThemeName";
 static NSString *kGeneralUIChange = @"DVTFontAndColorGeneralUISettingsChangedNotification";
-static NSString *kSourceTextChange = @"DVTFontAndColorSourceTextSettingsChangedNotification";
 static NSString *kFileChange = @"transition from one file to another";
 
 typedef NS_ENUM(NSInteger, LTFileType) {
@@ -21,6 +22,7 @@ typedef NS_ENUM(NSInteger, LTFileType) {
     LTFileType currentFileType;
     NSString *objcTheme;
     NSString *swiftTheme;
+    BOOL fileChangeInProgress;
 }
 
 + (void)pluginDidLoad:(NSBundle *)plugin {
@@ -39,16 +41,12 @@ typedef NS_ENUM(NSInteger, LTFileType) {
 
 - (id)initWithBundle:(NSBundle *)plugin {
     if (self = [super init]) {
-        // reference to plugin's bundle, for resource access
         self.bundle = plugin;
-//        DVTPreferenceSetManager *mgr = [DVTFontAndColorTheme preferenceSetsManager];
-//        mgr.currentPreferenceSet = mgr.userPreferenceSets.firstObject;
-//        NSLog(@"%@", ((DVTPreferenceSetManager *)[DVTFontAndColorTheme preferenceSetsManager]).availablePreferenceSets);
-//        NSLog(@"%@", ((DVTPreferenceSetManager *)[DVTFontAndColorTheme preferenceSetsManager]).userPreferenceSets);
-        // Listener for theme changes
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(themeDidChange:) name:kGeneralUIChange object:nil];
-        // Listener for file changes
+
+        [self loadPreferences];
+
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fileDidChange:) name:kFileChange object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(themeDidChange:) name:kGeneralUIChange object:nil];
     }
     return self;
 }
@@ -62,6 +60,7 @@ typedef NS_ENUM(NSInteger, LTFileType) {
     IMP imp = [loc methodForSelector:selector];
     id (*func)(id, SEL) = (void *)imp;
     NSString *fileURLString = func(loc, selector);
+
     LTFileType nextType;
 
     if ([[fileURLString substringWithRange:NSMakeRange(fileURLString.length - 2, 2)] isEqualToString:@".h"] ||
@@ -70,35 +69,93 @@ typedef NS_ENUM(NSInteger, LTFileType) {
     else if ([[fileURLString substringWithRange:NSMakeRange(fileURLString.length - 6, 6)] isEqualToString:@".swift"])
         nextType = LTFileTypeSwift;
 
-    if (!nextType || nextType == currentFileType) //NOTE: Non-native source files will maintain current theme
-        return;
+    if (!nextType || nextType == currentFileType)
+        return; //NOTE: Unsupported source files will use active theme
+
+    fileChangeInProgress = YES;
+
+    id mgr = self.prefsMgr;
+    SEL setCurrentPreferenceSet = NSSelectorFromString(@"setCurrentPreferenceSet:");
+    SEL availablePreferenceSets = NSSelectorFromString(@"availablePreferenceSets");
+
+    IMP availableThemes = [mgr methodForSelector:availablePreferenceSets];
+    func = (void *)availableThemes;
+
+    IMP setTheme = [mgr methodForSelector:setCurrentPreferenceSet];
+    void (*changeTheme)(id, SEL, id) = (void *)setTheme;
+    NSArray *themes = func(mgr, availablePreferenceSets);
 
     switch (nextType) {
         case LTFileTypeObjC:
-            // Set ObjC Theme
+            for (id theme in themes)
+                if ([[theme name] isEqualToString:objcTheme])
+                    changeTheme(mgr, setCurrentPreferenceSet, theme);
             break;
         case LTFileTypeSwift:
-            // Set Swift Theme
+            for (id theme in themes)
+                if ([[theme name] isEqualToString:swiftTheme])
+                    changeTheme(mgr, setCurrentPreferenceSet, theme);
             break;
     }
 
     currentFileType = nextType;
+    fileChangeInProgress = NO;
 }
 
 -(void)themeDidChange:(NSNotification *)note {
-    NSLog(@"Theme changed");
+    if (fileChangeInProgress)
+        return;
+
+    NSString *themeName = self.activeThemeName;
+    BOOL didUpdate = NO;
+
+    switch (currentFileType) {
+        case LTFileTypeObjC:
+            if (![objcTheme isEqualToString:themeName]) {
+                objcTheme = themeName;
+                didUpdate = YES;
+            }
+            break;
+        case LTFileTypeSwift:
+            if (![swiftTheme isEqualToString:themeName]) {
+                swiftTheme = themeName;
+                didUpdate = YES;
+            }
+            break;
+    }
+
+    if (didUpdate)
+        [self savePreferences];
 }
 
-- (NSMutableDictionary *)defaultz {
-    return [[[NSUserDefaults standardUserDefaults] persistentDomainForName:kPluginIdentifier] mutableCopy];
+- (id)prefsMgr {
+    Class DVTFontAndColorTheme = NSClassFromString(@"DVTFontAndColorTheme");
+    SEL selector = NSSelectorFromString(@"preferenceSetsManager");
+    IMP imp = [DVTFontAndColorTheme methodForSelector:selector];
+    id (*func)(id, SEL) = (void *)imp;
+    return func(DVTFontAndColorTheme, selector);
+}
+
+- (NSString *)activeThemeName {
+    Class DVTFontAndColorTheme = NSClassFromString(@"DVTFontAndColorTheme");
+    SEL currentTheme = NSSelectorFromString(@"currentTheme");
+    IMP impCurrentTheme = [DVTFontAndColorTheme methodForSelector:currentTheme];
+    id (*func)(id, SEL) = (void *)impCurrentTheme;
+    id newTheme = func(DVTFontAndColorTheme, currentTheme);
+    return [newTheme name];
 }
 
 - (void)loadPreferences {
-    //
+    NSDictionary *defaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName:kPluginIdentifier];
+    objcTheme = defaults[kObjcTheme] ?: self.activeThemeName;
+    swiftTheme = defaults[kSwiftTheme] ?: self.activeThemeName;
 }
 
 - (void)savePreferences {
-    //
+    NSMutableDictionary *defaults = [[NSUserDefaults standardUserDefaults] persistentDomainForName:kPluginIdentifier].mutableCopy;
+    [defaults setObject:objcTheme ?: swiftTheme forKey:kObjcTheme];
+    [defaults setObject:swiftTheme ?: objcTheme forKey:kSwiftTheme];
+    [[NSUserDefaults standardUserDefaults] setPersistentDomain:defaults forName:kPluginIdentifier];
 }
 
 - (void)dealloc {
